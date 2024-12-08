a0f7475064f88c5d324a06be7bfb263a5ae41466
PavneshKaushal <pavnesh.kaushal@gmail.com>
2024-12-07 20:43:28 -0500

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('reders without crashing', ()=>{});
+it('reders without crashing', ()=>{});
+it('reders without crashing', ()=>{});
+it('reders without crashing', ()=>{});
+it('reders without crashing', ()=>{});
